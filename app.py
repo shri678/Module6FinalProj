f0c635dac81fc8c8579731f08f3dc9bd88dfd0f5
shri678 <71198310+shri678@users.noreply.github.com>
2020-12-11 22:39:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    html.H1("Cricket Statistics"),style = {'text-align':'center'}
+    html.H1("Cricket Statistics"),style={'text-align':'center'}
 
     html.H3('Indian Premier League Statistics')
     html.Details([
