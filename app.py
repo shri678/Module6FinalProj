edc7a3aa5cd567375b4f7cf7c1ad1e9c9b4f2454
shri678 <71198310+shri678@users.noreply.github.com>
2020-12-12 12:11:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app.layout = html.Div([
     html.Div([
               html.Img("https://raw.githubusercontent.com/shri678/DataViz1/master/Daco_4377152.png"),
               html.H1("Cricket Statistics"),
-    ],style={'background-color':'SteelBlue', 'text-align':'center', 'color' : 'Gold'}),
+    ],style={'background-color':'WhiteSmoke', 'text-align':'center', 'color' : 'Gold'}),
     
 
     html.H3('Indian Premier League Statistics'),
