80c4d8b0d5a2ffc000629913fb25609f69d175b6
shri678 <71198310+shri678@users.noreply.github.com>
2020-12-11 23:16:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ server = app.server
 
 app.layout = html.Div([
     html.Div([
-              html.image("https://raw.githubusercontent.com/shri678/DataViz1/master/Daco_4377152.png"),
+              html.Img("https://raw.githubusercontent.com/shri678/DataViz1/master/Daco_4377152.png"),
               html.H1("Cricket Statistics"),
     ],style={'background-color':'lightblue', 'text-align':'center', 'color' : 'darkorange'}),
     
